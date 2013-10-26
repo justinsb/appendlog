59df858e9be7a629d21587f5d5d4fbbe11096c30
Dave Rusek <dave.rusek@gmail.com>
2013-10-25 22:32:28 -0600

    Initial

---
+++ src/main/java/org/robotninjas/barged/BargeD.java
@@ -0,0 +1,105 @@
+package org.robotninjas.barged;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.io.ByteStreams;
+import com.google.common.reflect.Reflection;
+import com.sun.grizzly.http.SelectorThread;
+import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
+import org.robotninjas.barge.*;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+@Path("/")
+public class BargeD {
+
+  public static String PACKAGE_OPT = "com.sun.jersey.config.property.packages";
+
+  private static Database database;
+
+  @GET
+  @Path("{key}")
+  @Produces(MediaType.APPLICATION_OCTET_STREAM)
+  public Response get(@PathParam("key") String key) {
+    byte[] data = database.get(key.getBytes());
+    return Response.ok(data).build();
+  }
+
+  @POST
+  @Path("{key}")
+  @Consumes(MediaType.APPLICATION_OCTET_STREAM)
+  public Response post(@PathParam("key") String key, InputStream value) throws IOException {
+    try {
+      database.put(key.getBytes(), ByteStreams.toByteArray(value));
+      return Response.ok().build();
+    } catch (InterruptedException e) {
+      return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+    } catch (NoLeaderException e) {
+      System.out.println("NO LEADER");
+      return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+    } catch (NotLeaderException e) {
+      System.out.println("REDIRECT");
+      Replica leader = e.getLeader();
+      InetSocketAddress address = (InetSocketAddress) leader.address();
+      URI uri = URI.create("http://" + address.getHostName() + ":" + address.getPort() + "/" + key);
+      System.out.println(uri);
+      return Response.seeOther(uri).build();
+    } catch (RaftException e) {
+      return Response.serverError().build();
+    }
+  }
+
+  public static void main(String... args) throws IOException {
+
+    final int port = Integer.parseInt(args[0]);
+
+    Replica local = Replica.fromString("localhost:" + (10000 + port));
+    List<Replica> members = Lists.newArrayList(
+      Replica.fromString("localhost:10001"),
+      Replica.fromString("localhost:10002"),
+      Replica.fromString("localhost:10003")
+    );
+    members.remove(local);
+
+    File logDir = new File(args[0]);
+    logDir.mkdir();
+
+    database = new Database();
+
+    final RaftService raft = RaftService.newBuilder()
+      .local(local)
+      .members(members)
+      .logDir(logDir)
+      .timeout(300)
+      .build(database);
+
+    database.init(raft);
+
+    raft.startAsync().awaitRunning();
+
+    final String baseUri = "http://localhost:" + (9990 + port) + "/";
+    final Map<String, String> initParams = Maps.newHashMap();
+
+    initParams.put(PACKAGE_OPT, Reflection.getPackageName(BargeD.class));
+
+    final SelectorThread selector = GrizzlyWebContainerFactory.create(baseUri, initParams);
+
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      @Override
+      public void run() {
+        selector.stopEndpoint();
+        raft.stopAsync().awaitTerminated();
+      }
+    });
+    
+  }
+}
---
+++ src/main/java/org/robotninjas/barged/TestClient.java
@@ -0,0 +1,36 @@
+package org.robotninjas.barged;
+
+import com.google.common.util.concurrent.RateLimiter;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import javax.ws.rs.core.MediaType;
+import java.nio.ByteBuffer;
+
+public class TestClient {
+
+  private static final String BASE_URL = "http://localhost:9991/";
+
+  public static void main(String... args) {
+
+    Client client = Client.create();
+    client.setFollowRedirects(true);
+
+    RateLimiter limiter = RateLimiter.create(1000);
+    for (long i = 0; i < 100000; i++) {
+      limiter.acquire();
+      ByteBuffer buffer = ByteBuffer.allocate(8);
+      buffer.putLong(i).rewind();
+      WebResource webResource = client.resource(BASE_URL);
+      ClientResponse response = webResource.path(Long.toString(i))
+        .entity(buffer.array(), MediaType.APPLICATION_OCTET_STREAM_TYPE)
+        .post(ClientResponse.class);
+
+      System.out.println(response.getStatus());
+    }
+
+
+  }
+
+}
